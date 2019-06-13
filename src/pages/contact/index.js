4f8fb109626071027f8c9c8d9321c0eba32f55f1
dax8it <alexcovo@gmail.com>
2019-06-12 22:11:43 -0400

    update contact form index.js
    '/?no-cache=1'

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -21,7 +21,7 @@ export default class Index extends React.Component {
   handleSubmit = e => {
     e.preventDefault()
     const form = e.target
-    fetch('/', {
+    fetch('/?no-cache=1', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
@@ -111,6 +111,7 @@ export default class Index extends React.Component {
             </div>
           </div>
         </section>
+
         <section>
         <div className="col-8">
           <h4 className="mt-4">Business Hours</h4>
