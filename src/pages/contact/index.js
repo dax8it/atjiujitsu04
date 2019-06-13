7abc14284fce194ea503c50d690475e2ab247934
dax8it <alexcovo@gmail.com>
2019-06-13 15:52:29 -0400

    updated contact page
    2 column responsive

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { navigate } from 'gatsby-link'
 import Layout from '../../components/Layout'
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope, faPhone, faHome } from "@fortawesome/free-solid-svg-icons"
+
 function encode(data) {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
@@ -36,45 +39,111 @@ export default class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Contact</h1>
 
-              <form
-                name="AT3-contact" 
-                method="POST" 
-                data-netlify="true"
-                data-netlify-honeypot="bot-field">
+<section>
+  <div className="row">
 
-                  
-
-                   {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="AT3-contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{' '}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <p>
-                  <label>Your Name: <input type="text" name="name" required/></label>   
-                </p>
-                <p>
-                  <label>Your Email: <input type="email" name="email" required/></label>
-                </p>
-                <p>
-                  <label>Message: <textarea name="message" required></textarea></label>
-                </p>
-
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
-
+              <div className="column">
+                <section className="section">
+                  <div className="container">
+                    <div className="content">
+                      <form
+                        name="AT3-contact" 
+                        method="POST" 
+                        data-netlify="true"
+                        data-netlify-honeypot="bot-field">
+                          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                          <input type="hidden" name="form-name" value="AT3-contact" />
+                          <div hidden>
+                            <label>
+                              Don’t fill this out:{' '}
+                              <input name="bot-field" onChange={this.handleChange} />
+                            </label>
+                          </div>
+                          <div className="col-lg-12 text-center">
+                            <h2 className="contactTitle">Write us</h2>
+                            <h3 className="subTitle">We would love to meet you!</h3>
+                            <br></br>
+                          </div>
+                        <p>
+                          <label>Your Name: <input type="text" name="name" required/></label>   
+                        </p>
+                        <p>
+                          <label>Your Email: <input type="email" name="email" required/></label>
+                        </p>
+                        <p>
+                          <label>Message: <textarea name="message"  rows="6" required></textarea></label>
+                        </p>
+                        <p>
+                          <button type="submit">Send</button>
+                        </p>
+                      </form>
+                    </div>
+                  </div>
+                </section>
               </div>
-            </div>
-        </section>
+
+
+          <div class="column">
+            <section>
+              <div className="container contacts">
+                <div className="row">
+                  <div className="col-lg-8 col-md-12">
+                  </div>
+                  <div className="col-lg-4 col-md-12">
+
+                    <section className="conatctSplit">
+
+                      <section>
+                        <div className="contact-method">
+                          <span className="icon">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                          </span>
+                          <h3>Email</h3>
+                          <a href="#">atjiujitsunyc@gmail.com</a>
+                        </div>
+                      </section>
+
+                      <section>
+                        <div className="contact-method">
+                          <span className="icon">
+                            <FontAwesomeIcon icon={faPhone} />
+                          </span>
+                          <h3>Phone</h3>
+                          <span>(917)-745-1772</span>
+                        </div>
+                      </section>
+
+                      <section>
+                        <div className="contact-method">
+                          <span className="icon">
+                            <FontAwesomeIcon icon={faHome} />
+                          </span>
+                          <h3>Address</h3>
+                          <span>
+                            80-22 Northern Blvd. 
+                            <br />
+                            Jackson Heights NY 11372
+                            <br />
+                            U.S.A
+                          </span>
+                        </div>
+                      </section>
+
+                    </section>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+
+  </div>
+</section>
+
+
+
+
+
       </Layout>
     )
   }
