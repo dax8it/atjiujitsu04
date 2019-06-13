10f72ec65e8c5d1f2bdf3073ce6509f6699d2d73
dax8it <alexcovo@gmail.com>
2019-06-12 23:15:47 -0400

    Update index.js
    returned contact to original

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,89 +1,115 @@
-import React from "react"
-import TemplateWrapper from "../../components/layout"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEnvelope, faPhone, faHome } from "@fortawesome/free-solid-svg-icons"
+import React from 'react'
+import { navigate } from 'gatsby-link'
+import Layout from '../../components/Layout'
 
-const Contact = props => (
-  <TemplateWrapper>
-    <div className="container contacts">
-      <div className="row">
-        <div className="col-lg-12 text-center">
-          <h1 className="contactTitle">Write us</h1>
-          <p className="subTitle">We would love to meet you!</p>
-        </div>
-        <div className="col-lg-8 col-md-12">
-          <form
-            className="contactForm"
-            name="contact"
-            method="post"
-            action="/success"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="bot-field" />
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" required />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" required />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="6" required />
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Clear" />
-              </li>
-            </ul>
-          </form>
-        </div>
-        <div className="col-lg-4 col-md-12">
-          <section className="contactSplit">
-            <section>
-              <div className="contact-method">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </span>
-                <h3>Email</h3>
-                <a href="#">atjiujitsunyc@gmail.com</a>
-              </div>
-            </section>
-            <section>
-              <div className="contact-method">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faPhone} />
-                </span>
-                <h3>Phone</h3>
-                <span>(917) 745-1772</span>
-              </div>
-            </section>
-            <section>
-              <div className="contact-method">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faHome} />
-                </span>
-                <h3>Address</h3>
-                <span>
-                  80-22 Northern Blvd.
-                  <br />
-                  Jackson Heights NY 11372
-                  <br />
-                  U.S.A
-                </span>
-              </div>
-            </section>
-          </section>
-        </div>
-      </div>
-    </div>
-  </TemplateWrapper>
-)
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isValidated: false }
+  }
 
-export default Contact
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <Layout>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <h1>Contact</h1>
+              <form
+                name="contact"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{' '}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'name'}>
+                    Your name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'text'}
+                      name={'name'}
+                      onChange={this.handleChange}
+                      id={'name'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'email'}>
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'email'}
+                      name={'email'}
+                      onChange={this.handleChange}
+                      id={'email'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'message'}>
+                    Message
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      name={'message'}
+                      onChange={this.handleChange}
+                      id={'message'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <button className="button is-link" type="submit">
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
