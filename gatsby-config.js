b2c0a952783fd5363f1f37401c8f50fe905355fd
dax8it <alexcovo@gmail.com>
2019-06-21 20:26:33 -0400

    Added Google Analytics
    Added gatsby-plugin-google-analytics. Updated gatsby-config.js with google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,32 @@ module.exports = {
     },
 
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-4799639-6",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+         head: false,
+        // Setting this parameter is optional
+         anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/admin/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "atjiujitsunyc.com",
+      },
+    },
+
+
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: "GatsbyJS",
