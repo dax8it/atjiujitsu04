8b7fd910cc953c098348faf4f58e2b068de20093
dax8it <alexcovo@gmail.com>
2019-06-22 12:29:37 -0400

    Update gatsby-config.js
    google analytics head: true

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
       options: {
         trackingId: "UA-4799639-6",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        // head: false,
+         head: true,
         // Setting this parameter is optional
         //  anonymize: true,
         // Setting this parameter is also optional
