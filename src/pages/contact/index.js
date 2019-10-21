7f3b417fa41f6fa18e66a605857397d2f80b3563
dax8it <alexcovo@gmail.com>
2019-10-21 14:12:08 -0400

    contact page
    update cleaned up

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -38,61 +38,82 @@ export default class Index extends React.Component {
 
   render() {
     return (
+
       <Layout>
 
-<section>
-  <div className="row1">
 
-              <div className="column2">
-                <section className="section">
-                  <div className="container">
-                    <div className="content">
-                      <form
-                        name="AT3-contact" 
-                        method="POST" 
-                        data-netlify="true"
-                        data-netlify-honeypot="bot-field">
-                          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                          <input type="hidden" name="form-name" value="AT3-contact" />
-                          <div hidden>
-                            <label>
-                              Don’t fill this out:{' '}
-                              <input name="bot-field" onChange={this.handleChange} />
-                            </label>
-                          </div>
-                          <div className="col-lg-12 text-center">
-                            <h2 className="contactTitle">Write us</h2>
-                            <h3 className="subTitle">We would love to meet you!</h3>
-                            <br></br>
-                          </div>
-                        <p>
-                          <label>Your Name: <input type="text" name="name" required/></label>   
-                        </p>
-                        <p>
-                          <label>Your Email: <input type="email" name="email" required/></label>
-                        </p>
-                        <p>
-                          <label>Message: <textarea name="message"  rows="6" required></textarea></label>
-                        </p>
-                        <p>
-                          <button type="submit">Send</button>
-                        </p>
-                      </form>
-                    </div>
-                  </div>
-                </section>
-              </div>
+<section className="section section--gradient">
+  <div className="container">
+    <div className="section">
+
+    <div className="columns is-10">
+        <div className="column is-10">
+          <h2 className="contactTitle has-text-weight-semibold is-size-3-mobile is-size-2-tablet is-size-1-widescreen">Write us</h2>
+          <h3 className="subTitle has-text-weight-semibold is-size-4-mobile is-size-3-tablet is-size-2-widescreen">We would love to meet you!</h3>
+          <br></br>
+        </div>
+        </div>
+
+          <div className="columns">
+
+            <div className="columns box">
+                    <form
+                      name="AT3-contact" 
+                      method="POST" 
+                      data-netlify="true"
+                      data-netlify-honeypot="bot-field">
+                        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                        <input type="hidden" name="form-name" value="AT3-contact" />
+                        <div hidden>
+                          <label>
+                            Don’t fill this out:{' '}
+                            <input name="bot-field" onChange={this.handleChange} />
+                          </label>
+                        </div>
+
+                        <div class="field">
+                        <label class="label">Your Name:</label> 
+                        <div class="control has-icons-left">
+                        <input type="text" class="textarea" rows="1" required/>  
+                      </div>
+                      </div>
+
+                      <div class="field">
+                      <label class="label">Your Email:</label>
+                        <div class="control">
+                        <input type="email" class="textarea" rows="1" name="email" required/>
+                        </div>
+                      </div>
+
+                      <div class="field">
+                      <label class="label">Message:</label>
+                        <div class="control">
+                        <textarea name="message"  class="textarea" rows="10" required></textarea>
+                        </div>
+                      </div>
+
+                      <div class="field is-grouped">
+                        <div class="control">
+                          <button class="button is-link" type="submit" >Submit</button>
+                        </div>
+                        <div class="control">
+                          <button class="button">Cancel</button>
+                        </div>
+                      </div>
 
 
-          <div class="column2">
+                    </form>
+            
+
+{/*
             <section>
               <div className="container contacts">
                 <div className="row">
-                  <div className="col-lg-8 col-md-12">
+                  <div className="column">
                   </div>
-                  <div className="col-lg-4 col-md-12">
+                  <div className="column">
 
-                    <section className="conatctSplit">
+                    <section className="contactSplit">
 
                       <section>
                         <div className="contact-method">
@@ -135,12 +156,15 @@ export default class Index extends React.Component {
                 </div>
               </div>
             </section>
+
+*/}
+
           </div>
 
-  </div>
+      </div>
+      </div>
+    </div>
 </section>
-
-
 
 
 
