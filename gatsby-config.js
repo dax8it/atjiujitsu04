c23ba05705f0893d7f3fd41cf3792955417b6c3d
dax8it <alexcovo@gmail.com>
2019-12-30 19:45:02 -0500

    Update gatsby-config.js
    delete analyticsID

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-4799639-6",
+        trackingId: "",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
          head: true,
         // Setting this parameter is optional
